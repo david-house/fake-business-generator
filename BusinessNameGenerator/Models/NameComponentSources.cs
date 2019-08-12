d04cbad22458ac0f8599c53bd58bf9d6ff947517
Dave House <david-house@users.noreply.github.com>
2019-08-12 16:08:11 -0400

    Combine all the data sources into one object and encapsulate rendering into that object.

---
+++ BusinessNameGenerator/Models/NameComponentSources.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessNameGenerator.Models
+{
+    public class NameComponentSources
+    {
+        public PlaceName[] PlaceNames { get; set; }
+        public string[] Surnames { get; set; }
+        public string[] Suffixes { get; set; }
+        public string[] Prefixes { get; set; }
+        public string[] FormsOfBusiness { get; set; }
+        public Dictionary<string, HashSet<string>> NamesUsedByState = new Dictionary<string, HashSet<string>>();
+        public Dictionary<string, HashSet<string>> NamesUsedByZIPCode = new Dictionary<string, HashSet<string>>();
+
+        private bool AddIfUnique(BusinessNameComponents businessNameComponents)
+        {
+            businessNameComponents.Tries++;
+
+            var state = businessNameComponents.PlaceName.ZipState;
+            var zipCode = businessNameComponents.PlaceName.ZipCode;
+
+            if (!NamesUsedByState.ContainsKey(state))
+                NamesUsedByState[state] = new HashSet<string>();
+            var namesForState = NamesUsedByState[state];
+
+            if (!NamesUsedByZIPCode.ContainsKey(zipCode))
+                NamesUsedByZIPCode[zipCode] = new HashSet<string>();
+            var namesForZIPCode = NamesUsedByZIPCode[zipCode];
+
+            if (namesForState.Contains(businessNameComponents[0]) ||
+                namesForZIPCode.Contains(businessNameComponents[1]))
+                return false;
+
+            namesForState.Add(businessNameComponents[0]);
+            namesForZIPCode.Add(businessNameComponents[1]);
+
+            return true;
+        }
+
+        private Random Random { get; set; } = new Random();
+
+        public BusinessNameComponents GenerateNewBusinessName()
+        {
+            while (true)
+            {
+                var bnc = GetRandomBusinessName();
+
+                if (AddIfUnique(bnc))
+                    return bnc;
+            }
+
+
+        }
+
+        private BusinessNameComponents GetRandomBusinessName()
+        {
+            return new BusinessNameComponents()
+            {
+                PlaceName = PlaceNames[Random.Next(PlaceNames.Length)],
+                Surname1 = Surnames[Random.Next(Surnames.Length)],
+                Surname2 = Surnames[Random.Next(Surnames.Length)],
+                Prefix = Prefixes[Random.Next(Prefixes.Length)],
+                Suffix = Suffixes[Random.Next(Suffixes.Length)],
+                LegalWord = FormsOfBusiness[Random.Next(FormsOfBusiness.Length)],
+                NameTemplateIds = new int[]
+                {
+                    Random.Next(5),
+                    Random.Next(5),
+                    Random.Next(5)
+                }
+            };
+
+            
+        }
+    }
+}
