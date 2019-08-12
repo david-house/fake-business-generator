c16b18e69d5f7923aed1cc833be5b3054be45849
Dave House <david-house@users.noreply.github.com>
2019-08-12 16:07:37 -0400

    Add complete PlaceName object instead of string, remove initializer for the random name template ID array

--- BusinessNameGenerator/Models/BusinessNameComponents.cs
+++ BusinessNameGenerator/Models/BusinessNameComponents.cs
@@ -1,31 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BusinessNameGenerator.Models
 {
     public class BusinessNameComponents
     {
-        public string PlaceName { get; set; }
+        public PlaceName PlaceName { get; set; }
         public string LegalWord { get; set; }
         public string Surname1 { get; set; }
         public string Surname2 { get; set; }
         public string Prefix { get; set; }
         public string Suffix { get; set; }
-        public int[] NameTemplateIds { get; set; } = new int[] {0, 0, 0};
+        public int[] NameTemplateIds { get; set; }
+        public int Tries { get; set; }
 
         public string GetNameFromTemplate(int templateId)
         {
             switch (templateId)
             {
                 case 0:
-                    return $"{Prefix} {Suffix} of {PlaceName} {LegalWord}";
+                    return $"{Prefix} {Suffix} of {PlaceName.Name} {LegalWord}";
                 case 1:
                     return $"{Surname1[0]}&{Surname2[0]} {Suffix} {LegalWord}";
                 case 2:
                     return $"{Surname1} {Suffix}";
                 case 3:
-                    return $"{PlaceName} {Suffix} {LegalWord}";
+                    return $"{PlaceName.Name} {Suffix} {LegalWord}";
                 case 4:
                     return $"{Surname1} {Prefix} {Suffix}";
                 default:
@@ -34,6 +33,5 @@ namespace BusinessNameGenerator.Models
         }
 
         public string this[int templateNumber] => GetNameFromTemplate(NameTemplateIds[templateNumber]);
-
     }
 }
