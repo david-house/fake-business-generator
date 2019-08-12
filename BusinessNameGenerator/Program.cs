87d06ba87739776497353006f08c5f2859d9d76b
Dave House <david-house@users.noreply.github.com>
2019-08-11 20:53:41 -0400

    Added code to generate full business names.

--- BusinessNameGenerator/Program.cs
+++ BusinessNameGenerator/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Runtime.ExceptionServices;
 using BusinessNameGenerator.Models;
 
 namespace BusinessNameGenerator
 {
     class Program
     {
-        static string 
         static void Main(string[] args)
         {
             var placeNames = PlaceNameLoader.Load("Data/place_names.txt");
             var numberOfPlaceNames = placeNames.Length;
-
+            
             var surnames = SurnameLoader.Load("Data/surnames_us.txt");
             var numberOfSurnames = surnames.Length;
 
@@ -21,15 +22,33 @@ namespace BusinessNameGenerator
             var prefixes = File.ReadAllLines("Data/business_prefixes.txt");
             var numberOfPrefixes = prefixes.Length;
 
+            var formsOfBusiness = File.ReadAllLines("Data/legal_control_words.txt");
+            var numberOfFormsOfBusiness = formsOfBusiness.Length;
+
             var random = new Random();
+            
 
             for (int i = 0; i < 100; i++)
             {
-                var placeName = placeNames[random.Next(numberOfPlaceNames)];
-                var surname = surnames[random.Next(numberOfSurnames)];
-                var suffix = suffixes[random.Next(numberOfSuffixes)];
-                var prefix = prefixes[random.Next(numberOfPrefixes)];
-                Console.WriteLine($"{placeName}, {surname}, {suffix}, {prefix}");
+                var businessName = new BusinessNameComponents()
+                {
+                    PlaceName = placeNames[random.Next(numberOfPlaceNames)].Name,
+                    Surname1 = surnames[random.Next(numberOfSurnames)],
+                    Surname2 = surnames[random.Next(numberOfSurnames)],
+                    Prefix = prefixes[random.Next(numberOfPrefixes)],
+                    Suffix = suffixes[random.Next(numberOfSuffixes)],
+                    LegalWord = formsOfBusiness[random.Next(numberOfFormsOfBusiness)],
+                    NameTemplateIds = new int[]
+                    {
+                        random.Next(5),
+                        random.Next(5),
+                        random.Next(5)
+                    }
+                };
+
+ 
+
+                Console.WriteLine($"{businessName[0]} {businessName[1]} {businessName[2]}");
             }
         }
     }
