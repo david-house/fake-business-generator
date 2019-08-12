1a6b650873b706cc05683272fa3003ff449dc9e2
Dave House <david-house@users.noreply.github.com>
2019-08-12 16:08:48 -0400

    Make city title cased. Input data is all upper case.

--- BusinessNameGenerator/Models/PlaceNames.cs
+++ BusinessNameGenerator/Models/PlaceNames.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace BusinessNameGenerator.Models
 {
@@ -12,8 +13,10 @@ namespace BusinessNameGenerator.Models
         public string ZipCode { get; set; }
         public string ZipState { get; set; }
         public string ZipCity { get; set; }
+        private TextInfo TextInfo = new CultureInfo("en-US", false).TextInfo;
 
         public PlaceName() { }
+        
 
         public PlaceName(string[] elements)
         {
@@ -22,8 +25,8 @@ namespace BusinessNameGenerator.Models
             Latitude = Convert.ToSingle(elements[2]);
             Longitude = Convert.ToSingle(elements[3]);
             ZipCode = elements[4];
-            ZipState = elements[5];
-            ZipCity = elements[6];
+            ZipState = elements[6];
+            ZipCity = TextInfo.ToTitleCase(elements[5].ToLower());
         }
 
         public override string ToString()
